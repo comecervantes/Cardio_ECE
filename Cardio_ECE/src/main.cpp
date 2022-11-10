1845224662224a43242229eb202c2b8902003bb6
comecervantes <117734730+comecervantes@users.noreply.github.com>
2022-11-10 18:55:12 +0100

    Add the oled

--- Cardio_ECE/src/main.cpp
+++ Cardio_ECE/src/main.cpp
@@ -1,20 +1,42 @@
 #include <Arduino.h>
 
+//oled
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+const int pot = A3;
+const int boutton = 6; 
+
+int etatbt; 
+int valeur_potentiometre; 
+
+//oled 
+//bpm 
+
 #define nombre_valeurs 4
 #define seuil 4
 
-int ledV = 2;
-int ledJ = 4;
-int ledR = 7;
+const int ledV = 2;
+const int ledJ = 3;
+const int ledR = 4;
 
-int buzzer = 9; 
+const int buzzer = 5; 
 
-int iDuino = A7;
+const int iDuino = A7;
 
 float valeurs[nombre_valeurs], somme, derniere_valeur, graphe, debut, premier, second, troisieme, valeur_precedente, bpm, liste_valeurs_bpm[2];
 long int actuel, i, dernier_bpm;
 int bpm_min = 50, bpm_max = 180, nombre_pics, n, note; 
 bool pic;
+
+//bpm 
 
 void setup() {
 
@@ -22,15 +44,28 @@ void setup() {
   pinMode(ledJ, OUTPUT);
   pinMode(ledR, OUTPUT);
 
+  pinMode(boutton, INPUT);
+
 digitalWrite(ledV, LOW);
 digitalWrite(ledJ, LOW);
 digitalWrite(ledR, LOW);
 
-    Serial.begin(9600);
+  if(!display.begin(SSD1306_SWITCHCAPVCC,0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+  for(;;);  } 
+  if(!display.begin(SSD1306_SWITCHCAPVCC,0x3D)) {
+    Serial.println(F("SSD1306 allocation failed"));
+  for(;;);  } 
+
+    Serial.begin(115200);
+
 }
 
 void loop ()
 {
+
+  etatbt = digitalRead(boutton); 
+
     for (int i = 0; i < nombre_valeurs; i++)
       valeurs[i] = 0;
     somme = 0;
@@ -120,10 +155,58 @@ void loop ()
     tone(buzzer, 1489);
 
   }
+  display.clearDisplay(); 
+
+  Wire.beginTransmission(0x3C);
+  display.begin(SSD1306_SWITCHCAPVCC,0x3C);
+ 
+    display.setTextSize(1); //je definie la taille du texte et la position de celui ci 
+    display.setTextColor(WHITE);
+        display.setCursor(15,35);
+
+        display.print("Tension: ");
+            display.setTextSize(2);
+  Wire.endTransmission();
+
+  display.begin(SSD1306_SWITCHCAPVCC,0x3D);
+  Wire.beginTransmission(0x3D);
+
+    display.setTextSize(1); //je definie la taille du texte et la position de celui ci 
+    display.setTextColor(WHITE);
+        display.setCursor(15,35);
+
+        display.print("Voltage: ");
+            display.setTextSize(2);
+  Wire.endTransmission();
 
     Serial.println(bpm);
 
 delay(10);
 
     }
+  
+    display.clearDisplay(); 
+
+  Wire.beginTransmission(0x3C);
+  display.begin(SSD1306_SWITCHCAPVCC,0x3C);
+ 
+    display.setTextSize(1); //je definie la taille du texte et la position de celui ci 
+    display.setTextColor(WHITE);
+        display.setCursor(15,35);
+
+        display.print("Tension: ");
+
+  Wire.endTransmission();
+
+  display.begin(SSD1306_SWITCHCAPVCC,0x3D);
+  Wire.beginTransmission(0x3D);
+
+    display.setTextSize(1); //je definie la taille du texte et la position de celui ci 
+    display.setTextColor(WHITE);
+        display.setCursor(15,35);
+
+        display.print("Voltage: ");
+
+  Wire.endTransmission();
+
 }
